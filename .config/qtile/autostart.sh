19b8c85056d78c3e41e4a4bf9c70f436c641a8f3
d33vliter <iandupuy1998@gmail.com>
2023-12-03 22:45:33 -0500

    Update autostart.sh

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,1 +1,29 @@
+#!/usr/bin/env bash
 
+#Monitor
+xrandr --dpi 94
+
+#wallpaper
+pkill nitrogen
+nitrogen --restore
+
+#picom
+pkill picom
+picom -f -D 2 & 
+
+#clipit
+pkill parcellite
+parcellite &
+
+#Numkey
+numlockx on
+
+#Cursor
+xsetroot -cursor_name left_ptr
+
+#Keyboard
+setxkbmap -layout us -variant intl
+
+#polkit
+pkill /usr/bin/lxpolkit
+/usr/bin/lxpolkit &
